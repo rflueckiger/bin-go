3316e9d73a57335620878f94307c52f65271ba8a
Roman Flueckiger <rflueckiger@inventage.com>
2025-06-11 09:49:28 +0200

    Add base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bin-go/',
   plugins: [VitePWA({
     registerType: 'prompt',
     injectRegister: false,
